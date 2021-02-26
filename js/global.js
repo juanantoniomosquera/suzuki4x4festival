d0e4878237d9ced0497c51f8a110b637455642bf
Juan Antonio Mosquera <juanantoniomosquera@juanantoniomosquera.com>
2021-02-26 13:40:31 +0100

    primera version de web

--- js/global.js
+++ js/global.js
@@ -6,14 +6,14 @@ $( function() {
 // Add background image
 		$.backstretch('images/road2.jpg');
 		
-        var endDate = "September  10, 2021 10:00:00";
+        var endDate = "September  17, 2021 10:00:00";
 
         $('.countdown.simple').countdown({ date: endDate });
 
         $('.countdown.styled').countdown({
           date: endDate,
           render: function(data) {
-            $(this.el).html("<div>" + this.leadingZeros(data.days, 3) + " <span>days</span></div><div>" + this.leadingZeros(data.hours, 2) + " <span>hrs</span></div><div>" + this.leadingZeros(data.min, 2) + " <span>min</span></div><div>" + this.leadingZeros(data.sec, 2) + " <span>sec</span></div>");
+            $(this.el).html("<div>" + this.leadingZeros(data.days, 3) + " <span>días</span></div><div>" + this.leadingZeros(data.hours, 2) + " <span>hrs</span></div><div>" + this.leadingZeros(data.min, 2) + " <span>min</span></div><div>" + this.leadingZeros(data.sec, 2) + " <span>seg</span></div>");
           }
         });
 
